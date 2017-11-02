01b8518c86a6b28eb08399a5d81206fb85bd37f9
xkf163 <572571573@qq.com>
2017-11-02 23:41:41 +0800

    重构调整2

--- ff-projects-film-jpa/src/main/java/ff/projects/crawler/DouBanProcessor.java
+++ ff-projects-film-jpa/src/main/java/ff/projects/crawler/DouBanProcessor.java
@@ -5,14 +5,12 @@ import ff.projects.entity.Person;
 import ff.projects.service.FilmService;
 import ff.projects.service.PersonService;
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,32 +54,37 @@ public class DouBanProcessor implements PageProcessor {
             //%7B%22distinct_id%22%3A%20%2215cf2586637111-09cc812eb1fc81-701238-2a3000-15cf258663828b%22%2C%22%24id%22%3A%20%22163054123%22%2C%22initial_view_time%22%3A%20%221498714502%22%2C%22initial_referrer%22%3A%20%22https%3A%2F%2Fmovie.douban.com%2Fsubject_search%3Fsearch_text%3D%25E7%2581%25AB%25E9%2594%2585%25E8%258B%25B1%25E9%259B%2584%26cat%3D1002%22%2C%22initial_referrer_domain%22%3A%20%22movie.douban.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201498717202%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201498717202%7D;
             // UM_distinctid=15cf2586637111-09cc812eb1fc81-701238-2a3000-15cf258663828b; ap=1; ue="xkf99@qq.com"; dbcl2="163054123:chmaQ1NsVJU"; ck=t0ID; _pk_id.100001.4cf6=a90e45eb3a458ed3.1498541166.30.1498897875.1498881693.; _pk_ses.100001.4cf6=*; __utma=30149280.561785331.1498541167.1498880150.1498896335.30;
             // __utmb=30149280.0.10.1498896335; __utmc=30149280; __utmz=30149280.1498541167.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=30149280.16305; __utma=223695111.754876944.1498541167.1498880150.1498896335.30; __utmb=223695111.0.10.1498896335; __utmc=223695111; __utmz=223695111.1498541167.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); push_noty_num=0; push_doumail_num=0; _vwo_uuid_v2=D8570F0119A0D26A171627FC7CD214FE|f9fd83098aac087eb826bd151219de18
-            .addCookie("CNZZDATA1258025570","427171256-1498714502-https%253A%252F%252Fmovie.douban.com%252F%7C1498714502")
-            .addCookie("__yadk_uid","XW2KSzLfC1NNzBidnF0W0DDZ6LkeuLOW")
-            .addCookie("_pk_id.100001.4cf6","a90e45eb3a458ed3.1498541166.30.1498897875.1498881693.")
+            .addCookie("viewed","3135476")
+            .addCookie("bid","1dhQuoaH0uY")
+            .addCookie("gr_user_id","922cd5e7-60dc-4640-92ac-f15bf31d7a41")
+            .addCookie("_vwo_uuid_v2","8348C22453371F63096941310C092747|716ec0c3985d6c23f6fb872987c154cd")
+            .addCookie("__utma","30149280.35513765.1506434677.1509619183.1509635285.7")
+            .addCookie("__utmz","30149280.1509635285.7.3.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/login")
+            .addCookie("_pk_ref.100001.4cf6","%5B%22%22%2C%22%22%2C1509635282%2C%22https%3A%2F%2Faccounts.douban.com%2Flogin%3Falias%3Dxkf99%2540qq.com%26redir%3Dhttps%253A%252F%252Fmovie.douban.com%252F%26source%3DNone%26error%3D1013%22%5D")
+            .addCookie("_pk_id.100001.4cf6","5d585618949ed972.1506434680.7.1509636307.1509619192.")
+            .addCookie("__yadk_uid","HhhHmaMNwKkDGee1buMnf6U6X5Ej1FQB")
+            .addCookie("ll","118172")
             .addCookie("_pk_ses.100001.4cf6","*")
-            .addCookie("ps","y")
-            .addCookie("UM_distinctid","15cf2586637111-09cc812eb1fc81-701238-2a3000-15cf258663828b")
-            .addCookie("__utma","30149280.561785331.1498541167.1498880150.1498896335.30")
-            .addCookie("__utmb","30149280.0.10.1498896335")
             .addCookie("__utmc","30149280")
-            .addCookie("__utmv","30149280.16305")
-            .addCookie("__utmz","30149280.1498541167.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)")
-            .addCookie("_vwo_uuid_v2","D8570F0119A0D26A171627FC7CD214FE|f9fd83098aac087eb826bd151219de18")
-            .addCookie("ap","1")
-            .addCookie("bid","tPZ_SBQRBFY")
-            .addCookie("cn_d6168da03fa1ahcc4e86_dplus","%7B%22distinct_id%22%3A%20%2215cf2586637111-09cc812eb1fc81-701238-2a3000-15cf258663828b%22%2C%22%24id%22%3A%20%22163054123%22%2C%22initial_view_time%22%3A%20%221498714502%22%2C%22initial_referrer%22%3A%20%22https%3A%2F%2Fmovie.douban.com%2Fsubject_search%3Fsearch_text%3D%25E7%2581%25AB%25E9%2594%2585%25E8%258B%25B1%25E9%259B%2584%26cat%3D1002%22%2C%22initial_referrer_domain%22%3A%20%22movie.douban.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201498717202%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201498717202%7D")
-            .addCookie("dbcl2","163054123:chmaQ1NsVJU")
-            .addCookie("ck","t0ID")
+            .addCookie("ps","y")
             .addCookie("ue","xkf99@qq.com")
-            .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+            .addCookie("__utmb","223695111.0.10.1509635285")
+            .addCookie("push_noty_num","0")
+            .addCookie("push_doumail_num","0")
+            .addCookie("ap","1")
+            .addCookie("dbcl2","163054123:OPWsXSJ6OhU")
+            .addCookie("ck","rlyv")
+            .addCookie("as","https://movie.douban.com/")
+
+            .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .addHeader("Accept-Encoding", "gzip, deflate, br")
             .addHeader("Accept-Language", "zh-CN,zh;q=0.8")
             .addHeader("Connection", "keep-alive")
             .addHeader("Referer", "http://movie.douban.com/")
             .setSleepTime(10000).setRetryTimes(3)
             .setCharset("utf-8")
-            .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36");
+            .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36");
+           // .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0");
 
 
 
@@ -97,7 +100,7 @@ public class DouBanProcessor implements PageProcessor {
             if (filmService.needCrawler(f)) {
                 //完整提取film信息
                 f = filmService.extractFilmSecondFromCrawler(page,f);
-                filmService.save(f);
+                //filmService.save(f);
                 newFilmList.add(f);
                 //从页面发现后续的url地址来抓取
                 //1)当前电影所有人物的url
@@ -117,7 +120,7 @@ public class DouBanProcessor implements PageProcessor {
             if(personService.needCrawler(p)){
                 //完整提取person信息
                 p = personService.extractFilmSecondFromCrawler(page);
-                personService.save(p);
+                //personService.save(p);
                 newPersonList.add(p);
                 if(!this.singleCrawler){
                     //最受欢迎5部
--- ff-projects-film-jpa/src/main/java/ff/projects/service/impl/CrawlerServiceImpl.java
+++ ff-projects-film-jpa/src/main/java/ff/projects/service/impl/CrawlerServiceImpl.java
@@ -1,14 +1,16 @@
 package ff.projects.service.impl;
 
-import ff.projects.common.ResultBean;
 import ff.projects.crawler.DouBanProcessor;
+import ff.projects.entity.Film;
+import ff.projects.entity.Person;
 import ff.projects.service.CrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.Spider;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * @Author:xukangfeng
@@ -21,6 +23,9 @@ public class CrawlerServiceImpl implements CrawlerService {
     @Autowired
     DouBanProcessor douBanProcessor;
 
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Override
     public Object[] running(String mutil, String singleFilmUrl, String thread, String homepage) {
         douBanProcessor.setSingleCrawler(true);
@@ -31,6 +36,31 @@ public class CrawlerServiceImpl implements CrawlerService {
         if ("1".equals(homepage))
             singleFilmUrl= "https://movie.douban.com/";
         Spider.create(douBanProcessor).addUrl(singleFilmUrl).thread(Integer.parseInt(thread)).run();
+
+        //批量保存
+        int size = douBanProcessor.newPersonList.size();
+        for(int i = 0;i<size;i++){
+            Person person = douBanProcessor.newPersonList.get(i);
+            entityManager.persist(person);
+            if(i % 50 == 0 || i==size-1){
+                entityManager.flush();
+                entityManager.clear();
+            }
+
+        }
+        size = douBanProcessor.newFilmList.size();
+        for (int i = 0;i<size;i++){
+            Film film = douBanProcessor.newFilmList.get(i);
+            entityManager.persist(film);
+            if(i % 50 == 0 || i==size-1){
+                entityManager.flush();
+                entityManager.clear();
+            }
+
+        }
+
+
+
         return new Object[]{douBanProcessor.newFilmList,douBanProcessor.newPersonList};
     }
 }
