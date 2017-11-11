01371f97ef3b433b12e3b9eb8917bc1f38b95b57
xkf163 <tiagoxu87445294g>
2017-11-11 11:15:31 +0800

    更改pom工程名为film-jpa

--- ff-projects-film-jpa/src/main/java/ff/projects/controller/IndexController.java
+++ ff-projects-film-jpa/src/main/java/ff/projects/controller/IndexController.java
@@ -3,10 +3,7 @@ package ff.projects.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -40,6 +37,70 @@ public class IndexController {
 
 
 
+/*
+
+
+    /**
+     * @Author: xukangfeng
+     * @Description 找出没有Film关联的Media，然后以media。namechn即电影中文名为关键字去豆瓣网搜索电影，再爬取下来
+     * @Date : 17:05 2017/10/27
+     */
+//    @GetMapping(value = "/crawler/patch")
+//    public String crawlerPatch(@RequestParam(value = "thread" ,defaultValue = "1") String thread){
+//
+//        String rootUrl = "https://movie.douban.com/subject_search?search_text=";
+//        //查询语句准备
+//        QMediaVO qMediaVO = QMediaVO.mediaVO;
+//        QMediaVOFilmVO qMediaVOFilmVO = QMediaVOFilmVO.mediaVOFilmVO;
+//
+//        List<Predicate> predicates = new ArrayList<>();
+//        predicates.add(qMediaVO.id.notIn(JPAExpressions.select(qMediaVOFilmVO.mediaVOId).from(qMediaVOFilmVO)));
+//        predicates.add(qMediaVO.media.deleted.eq(0));
+//        //Predicate predicate = qMediaVO.id.notIn(JPAExpressions.select(qMediaVOFilmVO.mediaVOId).from(qMediaVOFilmVO)).and(qMediaVO.media.deleted.eq(0));
+//        List<String> stringList = (List<String>) new JPAQueryFactory(entityManager)
+//                .selectFrom(qMediaVO)
+//                .select(qMediaVO.nameChn.prepend(rootUrl))
+//                .where(predicates.toArray(new Predicate[predicates.size()]))
+//                .fetch();
+//        //一系列url：格式rootUrl+nameCHN
+//        String[] urls = stringList.toArray(new String[stringList.size()]);
+//
+//        Spider.create(douBanProcessor).addUrl(urls).thread(Integer.parseInt(thread)).run();
+//        return "redirect:/";
+//    }
+
+
+
+
+
+
+
+    /**
+     * 补救：豆瓣评分 评分人数
+     * @param thread
+     * @param gatherUrl
+     * @return
+     */
+//    @GetMapping(value = "/crawler/patch/1/{thread}")
+//    public String crawlerPatchA(@PathVariable(name = "thread") String thread, @RequestParam (value = "url",required = false) String gatherUrl){
+//
+//        String rootUrl = "https://movie.douban.com/subject/";
+//        //查询语句准备
+//        QFilm qFilm = QFilm.film;
+//        Predicate predicate = qFilm.subjectOther.isNull().or(qFilm.doubanRating.isNull());
+//        List<String> stringList = (List<String>) new JPAQueryFactory(entityManager).selectFrom(qFilm).select(qFilm.doubanNo.prepend(rootUrl).append("/")).where(predicate).orderBy(qFilm.year.desc()).fetch();
+//        System.out.println("---> "+stringList.size());
+//        String[] urls = stringList.toArray(new String[stringList.size()]);
+//        if(null!=gatherUrl){
+//            urls = new String[1];
+//            urls[0]=gatherUrl;
+//        }
+//
+//
+//        Spider.create(douBanPatchProcessor).addUrl(urls).thread(Integer.parseInt(thread)).run();
+//        return "redirect:/";
+//
+//    }
 
 
 }
